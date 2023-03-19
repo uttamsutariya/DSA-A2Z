27b82ff1707e19803e9622ae8e47626d03057128
Uttam Sutariya <pateluttam171210@gmail.com>
2023-03-19 20:20:43 +0530

    Solved majority element

---
+++ _3_problems_on_arrays/medium/MajorityElement.java
@@ -0,0 +1,39 @@
+package _3_problems_on_arrays.medium;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MajorityElement {
+    public static void main(String[] args) {
+        int[] arr = {2,2,1,1,1,2,2};
+        System.out.println(majorityElement(arr));
+    }
+
+    public static int majorityElement(int[] nums) {
+
+//        map based solution
+//        Map<Integer, Integer> hm = new HashMap<>();
+//
+//        for (int num : nums)
+//            hm.put(num, hm.getOrDefault(num, 0) + 1);
+//
+//        for(Map.Entry<Integer, Integer> it: hm.entrySet()){
+//            if(it.getValue() > nums.length / 2){
+//                return it.getKey();
+//            }
+//        }
+//
+//        return -1;
+
+        int elem = nums[0];
+
+        int count = 0;
+
+        for (int num : nums) {
+            if(count == 0) elem = num;
+            count += (elem == num) ? 1 : -1;
+        }
+
+        return elem;
+    }
+}
